4c7f409e6d1d83978fabfafcb370eee60327e728
Matthew Wilding <mbwilding@gmail.com>
2025-01-06 17:10:12 +0800

    2025-01-06-17-10-12

--- containers/ssh-proxy/start.sh
+++ containers/ssh-proxy/start.sh
@@ -1,2 +1,9 @@
-echo $CERT > /tmp/certificate
+{
+  echo "-----BEGIN OPENSSH PRIVATE KEY-----"
+  echo "$CERT_PART_1" | sed 's/ /\n/g'
+  echo "$CERT_PART_2" | sed 's/ /\n/g'
+  echo "$CERT_PART_3" | sed 's/ /\n/g'
+  echo "$CERT_PART_4" | sed 's/ /\n/g'
+  echo "-----END OPENSSH PRIVATE KEY-----"
+} > /tmp/certificate
 ssh -D 1080 -N -o "ServerAliveInterval 60" -i /tmp/certificate ${USER}@${HOST} -p ${PORT}
